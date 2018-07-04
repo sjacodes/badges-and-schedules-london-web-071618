a956b3cee2e25afcc0962fba6da184226d15647e
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 19:27:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,25 +11,6 @@ end
   
   
   
-  
-  
-  list_of_names = []
-  names.each do |name|
-    list_of_names.push("Hello, my name is #{name}.")
-  end
-  return list_of_names
-end
-
-
-def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    badge_maker(name)
-  end
-
-end
-
-
-
 def assign_rooms(names)
   rooms = (1..7).to_a
   room_assignments = []
