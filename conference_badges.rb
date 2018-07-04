9f3622265f64db0c1096d1638651d986605d1f7c
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 19:22:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(names)
+  list_of_names = []
+  names.each do |name|
+    list_of_names.push("Hello, my name is #{name}.")
+  end
+  return list_of_names
+end
+
+def assign_rooms(names)
+  rooms = (1..7).to_a
+  room_assignments = []
+  
+  
+
+
+
+
+
+  
+      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
+    end
+
+  end
+
+  
